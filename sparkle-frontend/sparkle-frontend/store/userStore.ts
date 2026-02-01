f4c131cac0750f2d93daaa121428be1e91c0523d
Aina Adoption Oluwasomidotun <ainaadoption@gmail.com>
2026-02-01 23:45:30 +0100

    Update userStore.ts

--- sparkle-frontend/sparkle-frontend/store/userStore.ts
+++ sparkle-frontend/sparkle-frontend/store/userStore.ts
@@ -1,1 +1,22 @@
 
+#!filepath store/userStore.ts
+import { create } from "zustand";
+import { EmotionalStage } from "@/lib/types";
+
+interface UserState {
+  name: string;
+  // We store the user's overall current stage here
+  currentEmotionalStage: EmotionalStage; 
+  
+  // Actions to update the state
+  setName: (name: string) => void;
+  setEmotionalStage: (stage: EmotionalStage) => void;
+}
+
+export const useUserStore = create<UserState>((set) => ({
+  name: "Guest", // Default value
+  currentEmotionalStage: "Uninformed Optimism", // Default start
+  
+  setName: (name) => set({ name }),
+  setEmotionalStage: (stage) => set({ currentEmotionalStage: stage }),
+}));
